ec1eecdf9a6722659098ee75918d4339c7e896cd
Kapza <lukakapetanovic@icloud.com>
2024-11-06 14:49:39 +0100

    Completed functionality.

--- js.js
+++ js.js
@@ -4,42 +4,40 @@ let operator = null;
 const firstVisVal = document.getElementById("first-val");
 const secondVisVal = document.getElementById("second-val");
 let result = null;
+const screen = document.getElementById("screenId");
 
-const addition = (firstNum, nextNum) => {
-  return firstNum + nextNum;
-};
-
-const subtraction = (firstNum, nextNum) => {
-  return firstNum - nextNum;
-};
-
-const multiply = () => {
-  return firstNum * nextNum;
-};
-
-const divide = (firstNum, nextNum) => {
-  return firstNum / nextNum;
-};
+const addition = (firstNum, nextNum) => firstNum + nextNum;
+const subtraction = (firstNum, nextNum) => firstNum - nextNum;
+const multiply = () => firstNum * nextNum;
+const divide = (firstNum, nextNum) => firstNum / nextNum;
 
 const clear = () => {
   screen.innerHTML = "";
   firstNum = null;
   nextNum = null;
+  location.reload();
+  firstVisVal = "";
+  secondVisVal = "";
 };
+
+const resetCalc = document.getElementById("clear").addEventListener("click", clear);
 
 const operate = (firstNum, operator, nextNum) => {
   if (operator === "+") {
-    console.log(addition(firstNum, nextNum));
-    addition(firstNum, nextNum);
+    // console.log(addition(firstNum, nextNum));
+    return addition(firstNum, nextNum);
   }
   if (operator === "-") {
-    subtraction(firstNum, nextNum);
+    // console.log(addition(firstNum, nextNum));
+    return subtraction(firstNum, nextNum);
   }
   if (operator === "/") {
-    divide(firstNum, nextNum);
+    // console.log(addition(firstNum, nextNum));
+    return divide(firstNum, nextNum);
   }
-  if (operator === "*") {
-    multiply(firstNum, nextNum);
+  if (operator === "x") {
+    // console.log(addition(firstNum, nextNum));
+    return multiply(firstNum, nextNum);
   }
 };
 
@@ -65,3 +63,17 @@ const getOperator = (op) => {
 const equals = document.getElementById("equals").addEventListener("click", () => {
   operate(firstNum, operator, nextNum);
 });
+
+const getResult = () => {
+  if (firstNum != null && operator != null && nextNum != null) {
+    result = operate(firstNum, operator, nextNum);
+    screen.innerHTML = result;
+    console.log(result);
+
+    firstNum = result;
+    nextNum = null;
+    operator = null;
+  }
+};
+
+document.getElementById("equals").addEventListener("click", getResult);
